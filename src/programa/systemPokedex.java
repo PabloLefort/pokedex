6881fb8296f9edfdf24005ff7370a9da252689f1
koenigstaiger <dmalagueno85@gmail.com>
2016-11-15 00:20:21 -0300

    Agrego el metodo PokemonesPoderosos

--- src/programa/systemPokedex.java
+++ src/programa/systemPokedex.java
@@ -21,7 +21,7 @@ public class systemPokedex {
 	}
 
 	public ColaID OrdenarPokemones(TIPO t, String criterio){
-		pokemonID raiz = poke.getRaiz();
+		NodoPokemonABB raiz = poke.getRaiz();
 		int aux = raiz.getEspecie().compareTo(t);
 		while(aux != 0){
 			if(aux > 0){
@@ -57,6 +57,72 @@ public class systemPokedex {
 			break;
 		}
 		return pokemones;
+	}
+
+	public ColaPrioridad PokemonesPoderosos(int cantidad){
+		
+		//Creo la cola prioridad que sera el resultado del metodo
+		ColaPrioridad pokesPoderosos = new ColaPrioridad();
+		pokesPoderosos.InicializarCola();
+		
+		
+		ColaPrioridad aux = new ColaPrioridad();
+		aux.InicializarCola();
+		
+		//Me traigo el nodo principal del arbol
+		NodoPokemonABB raiz = poke.getRaiz();
+		
+		//Metodo para buscar la cantidad de pokemones del nodo y que recorra el arbol subsiguiente
+		//Me devuelve una colaPrioridad con todo ya ordenado
+		aux = TraigoPokes(raiz, cantidad, aux);
+		
+		int cont = 0;
+		while(!aux.ColaVacia() || cont <= cantidad){
+			
+			try {
+				
+				pokesPoderosos.AgregarElemento(aux.RecuperarMaxElemento(), aux.RecuperarMaxPrioridad());
+				aux.EliminarMaxPrioridad();
+				cont++;
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return pokesPoderosos;
+		
+	}
+	
+	private ColaPrioridad TraigoPokes(NodoPokemonABB raiz, int cantidad, ColaPrioridad resultado){
+		
+		if(raiz != null){
+			ColaPrioridad aux = raiz.getPokemones();
+			
+			int cont = 0;
+			while(!aux.ColaVacia() || cont <= cantidad){
+				
+				try {
+					
+					resultado.AgregarElemento(aux.RecuperarMaxElemento(), aux.RecuperarMaxPrioridad());
+					aux.EliminarMaxPrioridad();
+					cont++;
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			if(raiz.getHijoIzq() != null)
+				resultado = TraigoPokes(raiz.getHijoIzq(), cantidad, resultado);
+			
+			if(raiz.getHijoDer() != null)
+				resultado = TraigoPokes(raiz.getHijoDer(), cantidad, resultado);
+			
+		}
+		return resultado;
 	}
 	
 }
