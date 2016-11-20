797c36a5edc4664dfac19bff292e36a31f40ae31
Lefort Pablo <lefortpablo@gmail.com>
2016-11-20 15:10:27 -0300

    Refact system pokedex

--- src/programa/systemPokedex.java
+++ src/programa/systemPokedex.java
@@ -30,101 +30,130 @@ public class systemPokedex {
 			return buscarTDA(t, p.getRaiz().getHijoIzq());
 	}
 
-	public ColaID OrdenarPokemones(TIPO t, String criterio){
+	public ColaPrioridad<Pokemon> OrdenarPokemones(TIPO t, String criterio){
 		TDAPokedex raiz = this.buscarTDA(t, poke.getRaizTDA());
-		ColaID pokemones = new ColaID();
-		switch (criterio) {
-			case "Nombre":
-				
-			break;
-			
-			case "PC":
-				
-			break;
-			
-			case "PS":
-				
-			break;
-			
-			case "Ataque":
-				
-			break;
-
-			case "Defensa":
-				
-			break;
-			
-			case "Stamina":
-				
-			break;
-		}
-		return pokemones;
-	}
-
-	public ColaPrioridad PokemonesPoderosos(int cantidad){
-		
-		//Creo la cola prioridad que sera el resultado del metodo
-		ColaPrioridad pokesPoderosos = new ColaPrioridad();
-		pokesPoderosos.InicializarCola();
-		
-		
-		ColaPrioridad aux = new ColaPrioridad();
-		aux.InicializarCola();
-		
-		//Me traigo el nodo principal del arbol
-		NodoPokemonABB raiz = poke.getRaiz();
-		
-		//Metodo para buscar la cantidad de pokemones del nodo y que recorra el arbol subsiguiente
-		//Me devuelve una colaPrioridad con todo ya ordenado
-		aux = TraigoPokes(raiz, cantidad, aux);
-		
-		int cont = 0;
-		while(!aux.ColaVacia() || cont <= cantidad){
-			
+		ColaPrioridad<Pokemon> resultado = new ColaPrioridad<Pokemon>();
+		resultado.InicializarCola();
+		ColaPrioridad<Pokemon> pokemones = raiz.getRaiz().getPokemones();
+		while(!pokemones.ColaVacia()){
+			Pokemon aux;
 			try {
-				
-				pokesPoderosos.AgregarElemento(aux.RecuperarMaxElemento(), aux.RecuperarMaxPrioridad());
-				aux.EliminarMaxPrioridad();
-				cont++;
-				
+				aux = pokemones.RecuperarMaxElemento();
+				switch (criterio) {
+					case "Nombre":
+						resultado.AgregarElemento(aux, Integer.parseInt(aux.getNombre()));
+					break;
+					
+					case "PC":
+						resultado.AgregarElemento(aux, aux.getPuntosDeCombate());
+					break;
+					
+					case "PS":
+						resultado.AgregarElemento(aux, aux.getPuntosDeSalud());
+					break;
+					
+					case "Ataque":
+						resultado.AgregarElemento(aux, aux.getAtaque());
+					break;
+		
+					case "Defensa":
+						resultado.AgregarElemento(aux, aux.getDefensa());
+					break;
+					
+					case "Stamina":
+						resultado.AgregarElemento(aux, aux.getStamina());
+					break;
+				}
+				pokemones.EliminarMaxPrioridad();
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
-		return pokesPoderosos;
-		
+		return resultado;
 	}
 	
-	private ColaPrioridad TraigoPokes(NodoPokemonABB raiz, int cantidad, ColaPrioridad resultado){
-		
-		if(raiz != null){
+	public ColaPrioridad PokemonesDebiles(int cantidad){
+		ColaPrioridad pokemonesDebiles = new ColaPrioridad();
+		pokemonesDebiles.InicializarCola();
+		ColaPrioridad aux = new ColaPrioridad();
+		aux.InicializarCola();
+		NodoPokemonABB raiz = poke.getRaiz();
+		aux = TraerPokemones(raiz, cantidad, aux, false);
+		int cont = 0;
+		while(!aux.ColaVacia() || cont <= cantidad){
+			try {
+				pokemonesDebiles.AgregarElemento(aux.RecuperarMinElemento(), aux.RecuperarMinPrioridad());
+				aux.EliminarMinPrioridad();
+				cont++;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return pokemonesDebiles;
+	}
+
+	public ColaPrioridad PokemonesPoderosos(int cantidad){
+		ColaPrioridad pokesPoderosos = new ColaPrioridad();
+		pokesPoderosos.InicializarCola();
+		ColaPrioridad aux = new ColaPrioridad();
+		aux.InicializarCola();
+		NodoPokemonABB raiz = poke.getRaiz();
+		if(raiz == null)
+			return pokesPoderosos;
+		aux = this.TraerPokemones(raiz, cantidad, aux, true);
+		int cont = 0;
+		while(!aux.ColaVacia() || cont <= cantidad){
+			try {
+				pokesPoderosos.AgregarElemento(aux.RecuperarMaxElemento(), aux.RecuperarMaxPrioridad());
+				aux.EliminarMaxPrioridad();
+				cont++;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return pokesPoderosos;
+	}
+	
+	private ColaPrioridad TraerPokemones(NodoPokemonABB raiz, int cantidad, ColaPrioridad resultado, boolean poderosos){
+		if(poderosos){
 			ColaPrioridad aux = raiz.getPokemones();
 			
 			int cont = 0;
 			while(!aux.ColaVacia() || cont <= cantidad){
 				
 				try {
-					
 					resultado.AgregarElemento(aux.RecuperarMaxElemento(), aux.RecuperarMaxPrioridad());
 					aux.EliminarMaxPrioridad();
 					cont++;
-					
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
 			}
-			if(raiz.getHijoIzq() != null)
-				resultado = TraigoPokes(raiz.getHijoIzq(), cantidad, resultado);
 			
-			if(raiz.getHijoDer() != null)
-				resultado = TraigoPokes(raiz.getHijoDer(), cantidad, resultado);
+		} else {
+			ColaPrioridad aux = raiz.getPokemones();
 			
+			int cont = 0;
+			while(!aux.ColaVacia() || cont <= cantidad){
+				
+				try {
+					resultado.AgregarElemento(aux.RecuperarMinElemento(), aux.RecuperarMinPrioridad());
+					aux.EliminarMinPrioridad();
+					cont++;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		}
+		if(raiz.getHijoIzq() != null)
+			resultado = this.TraerPokemones(raiz.getHijoIzq().getRaiz(), cantidad, resultado, poderosos);
+		
+		if(raiz.getHijoDer() != null)
+			resultado = this.TraerPokemones(raiz.getHijoDer().getRaiz(), cantidad, resultado, poderosos);
+		
 		return resultado;
 	}
+
+
 	
 }
